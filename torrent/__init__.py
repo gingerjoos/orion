37b789d59f06f9b12aa078e7899038428cd36747
Anirudh Surendranath <gingerjoos@gmail.com>
2012-05-02 06:36:40 +0530

    latest changes

--- torrent/__init__.py
+++ torrent/__init__.py
@@ -2,22 +2,33 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from utils.tracker import read_torrent_url,read_torrent_file,get_tracker_info
+from os import getcwd
 app = Flask(__name__)
+
+# settings
+UPLOAD_FOLDER = getcwd() + 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/",methods=['GET','POST'])
 def hello():
     tracker_info = None
     if request.method == 'POST':
         try:
-            url = request.form['torrent_url']
-            torrent = read_torrent_url(url)
+            uploaded_file = request.files['torrent_file']
+            temp_filename = '/tmp/file.torrent'
+            uploaded_file.save(temp_filename)
+            torrent = read_torrent_file(temp_filename)
+            #filename = secure_filename(file_name.filename)
+            #torrent = read_torrent_file(filename)
         except:
-            file_name = request.form['torrent_file']
-            torrent = read_torrent_file(file_name)
+            try:
+                url = request.form['torrent_url']
+                torrent = read_torrent_url(url)
+            except:
+                pass
         tracker_info = get_tracker_info(torrent)
     return render_template('index.html',info = tracker_info)
 
-@app.route("/pg/")
-@app.route("/pg/<name>")
-def name_print(name='Anirudh'):
-    return render_template('name.html',name=name)
+@app.route("/pg/<view_file>")
+def static_page(view_file='about'):
+    return render_template(view_file+'.html')
