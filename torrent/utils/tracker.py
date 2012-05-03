b561715922644e5aef349286ad0165f2b5f46fe0
Anirudh S <gingerjoos@gmail.com>
2012-05-03 14:58:59 +0530

    style the results and changes to info passed to index view

--- torrent/utils/tracker.py
+++ torrent/utils/tracker.py
@@ -13,7 +13,9 @@ def parse_torrent(torrent):
     announce_list = [decoded_announce]
     try:
         announces = decoded['announce-list']
-        announce_list.extend(announces)
+        for tracker in announces:
+            if tracker != decoded_announce:
+                announce_list.extend(tracker)
     except:
         pass
     encoded_info = bencode.bencode(info)
@@ -31,7 +33,10 @@ def read_torrent_file(filename):
     return torrent
 
 def read_torrent_url(url):
-    response = urllib2.urlopen(url).read()
+    try:
+        response = urllib2.urlopen(url).read()
+    except:
+        return False
     return response
 
 
@@ -41,13 +46,22 @@ def get_peer_count(tracker,info_hash):
     for i in range(12):
         rand_str = rand_str + chr(random.randint(0,255))
     peer_id = urllib.quote_plus(rand_str)
+    info_hash = urllib.quote_plus(info_hash)
 
     url = tracker + "?info_hash=" + info_hash + "&peer_id=" + peer_id + "&port=9999&uploaded=0&downloaded=0&left=0&numwant=10&compact=1"
 
-    response = urllib2.urlopen(url).read()
+    try:
+        response = urllib2.urlopen(url).read()
+    except:
+        return False
     decoded_response = bencode.bdecode(response)
-
-    peer_count = {'seeders'  : decoded_response['complete'],
+    try:
+        decoded_response['complete']
+    except:
+        peer_count = decoded_response
+    else:
+        peer_count = {'tracker' : tracker,
+            'seeders'  : decoded_response['complete'],
             'leachers' : decoded_response['incomplete'],
             'interval' : decoded_response['interval'],}
     return peer_count
